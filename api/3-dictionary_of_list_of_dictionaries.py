051013a9db537f24b9df41bd37c63f83e508bcb1
Santiago E <santy.e@gmail.com>
2023-08-21 19:18:47 -0600

    Fix pep8

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -6,19 +6,24 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
+    api_url = "https://jsonplaceholder.typicode.com/"
 
-    users = requests.get(url + "users").json()
-    todos = requests.get(url + "todos").json()
+    users = requests.get(api_url + "users").json()
+    todos = requests.get(api_url + "todos").json()
 
     all_tasks = {}
 
     for user in users:
         user_id = user.get("id")
         username = user.get("username")
-        tasks = [{"username": username, "task": task.get("title"),
-                  "completed": task.get("completed")}
-                  for task in todos if task.get("userId") == user_id]
+        tasks = [
+            {
+                "username": username,
+                "task": task.get("title"),
+                "completed": task.get("completed")
+            }
+            for task in todos if task.get("userId") == user_id
+        ]
         all_tasks[user_id] = tasks
 
     with open("todo_all_employees.json", "w") as jsonfile:
