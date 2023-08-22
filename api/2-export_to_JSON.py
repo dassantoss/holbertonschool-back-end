61362a99bb8d65fd2b3db2f4da2a2ba47d3dec38
Santiago E <santy.e@gmail.com>
2023-08-21 19:03:44 -0600

    Add Script thats export to JSON

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Script thats gather data from an API and export to JSON"""
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+
+    user_id = argv[1]
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    todo = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+            "task": task.get("title"),
+            "completed": task.get("completed"),
+            "username": user.get("username")} for task in todo]}, jsonfile)
